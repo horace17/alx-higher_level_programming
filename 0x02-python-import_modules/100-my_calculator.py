c7102caeedbb190ec6a8e1869641e32b8aab48dd
horace17 <hocedrix2010@gmail.com>
2022-09-23 07:11:47 -0700

    Task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__"
+import calculator_1 *
+
+count = len(sys.argv)-1
+
+if count != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+if operator != "+,-,*,/":
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
