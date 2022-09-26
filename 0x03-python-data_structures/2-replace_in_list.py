df5d2ba6f960d25872cd0b7002892178cbfd1b1e
horace17 <hocedrix2010@gmail.com>
2022-09-26 05:03:47 -0700

    fixed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return my_list[idx]
-    elif idx >= len(my_list):
-        return my_list[idx]
+        return my_list(idx, element)
+    elif idx > len(my_list):
+        return my_list(idx, element)
     else:
         return my_list[idx]
