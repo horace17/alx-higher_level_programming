9813d5469cf1ff27ca7518a5f2a9b1a4761d19e0
horace17 <hocedrix2010@gmail.com>
2022-10-25 23:01:19 +0300

    Task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 
 '''Task 02 - 2. Append to a file'''
 
+
 def append_write(filename="", text=""):
     '''function that appends a string at the end of a text file (UTF8)
     and returns the number of characters added'''
